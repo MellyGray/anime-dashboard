1579064a3ec5c1c9d690549837e4d6daa57ba6bd
MellyGray <melinaabril@gmail.com>
2023-01-03 15:07:27 +0100

    Add score to animedashconfig type

--- src/anime_dash_config.ts
+++ src/anime_dash_config.ts
@@ -1,10 +1,10 @@
-
 export interface AnimeDashConfig {
 	widgets: {
 		id: number;
 		url: string;
 		title: string;
 		image_url: string;
+		score: number;
 	}[];
 }
 
@@ -15,18 +15,21 @@ export const config: AnimeDashConfig = {
 			url: "https://myanimelist.net/anime/32182/Mob_Psycho_100",
 			title: "Mob Psycho 100",
 			image_url: "https://cdn.myanimelist.net/images/anime/8/80356.jpg",
+			score: 8.66,
 		},
 		{
 			id: 42307,
 			url: "https://myanimelist.net/anime/42307/Subarashiki_Kono_Sekai_The_Animation",
 			title: "Subarashiki Kono Sekai The Animation",
 			image_url: "https://cdn.myanimelist.net/images/anime/1016/111566.jpg",
+			score: 7.66,
 		},
 		{
 			id: 2904,
 			url: "https://myanimelist.net/anime/2904/Code_Geass__Hangyaku_no_Lelouch_R2",
 			title: "Code Geass: Hangyaku no Lelouch R2",
 			image_url: "https://cdn.myanimelist.net/images/anime/4/9391.jpg",
+			score: 5.66,
 		},
 	],
 };
