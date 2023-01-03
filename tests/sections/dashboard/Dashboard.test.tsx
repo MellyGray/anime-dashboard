cc7eaa68bd8373ef027e372000f8396210e5d6f3
MellyGray <melinaabril@gmail.com>
2023-01-03 12:10:47 +0100

    Fix lint

--- tests/sections/dashboard/Dashboard.test.tsx
+++ tests/sections/dashboard/Dashboard.test.tsx
@@ -1,15 +1,16 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
 import { Dashboard } from "../../../src/sections/dashboard/Dashboard";
 
 describe("Dashboard section", () => {
 	test("show all widgets", async () => {
-        render(<Dashboard></Dashboard>);
+		render(<Dashboard></Dashboard>);
 
-        const firstWidgetTitle = `Widget heading 1`;
+		const firstWidgetTitle = `Widget heading 1`;
 		const firstWidgetHeader = await screen.findByRole("heading", {
 			name: new RegExp(firstWidgetTitle, "i"),
 		});
 
 		expect(firstWidgetHeader).toBeInTheDocument();
-    });
+	});
 });
