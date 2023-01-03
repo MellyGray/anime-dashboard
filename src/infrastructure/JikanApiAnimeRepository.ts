06f2cf37fe95d1c8b20b72f4677cf31413e57d4b
MellyGray <melinaabril@gmail.com>
2023-01-03 20:16:29 +0100

    Add implementation JikanApiAnimeRepository

---
+++ src/infrastructure/JikanApiAnimeRepository.ts
@@ -0,0 +1,25 @@
+import { Anime } from "../domain/Anime";
+import { AnimeRepository } from "../domain/AnimeRepository";
+import { JikanApiResponse } from "./JikanApiResponse";
+
+export class JikanApiAnimeRepository implements AnimeRepository {
+	static readonly jikanUrl: string = "https://api.jikan.moe/v4/top/anime?filter=airing";
+
+	async search(): Promise<Anime[]> {
+		return fetch(JikanApiAnimeRepository.jikanUrl)
+			.then((response) => response.json())
+			.then((response) => {
+				const jikanApiResponse = response as JikanApiResponse;
+
+				return jikanApiResponse.data.map((anime) => {
+					return {
+						id: anime.mal_id,
+						url: anime.url,
+						title: anime.title,
+						imageUrl: anime.images[0].image_url,
+						score: anime.score,
+					};
+				});
+			});
+	}
+}
