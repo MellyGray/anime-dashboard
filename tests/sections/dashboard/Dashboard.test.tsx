f66eb96f8ba8840d79f652bbdb336b5e0ba4001d
MellyGray <melinaabril@gmail.com>
2023-01-03 16:16:24 +0100

    Add jikanApiResponse to unit tests

--- tests/sections/dashboard/Dashboard.test.tsx
+++ tests/sections/dashboard/Dashboard.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 
+import { jikanApiResponses } from "../../../src/jikan_api_responses";
 import { Dashboard } from "../../../src/sections/dashboard/Dashboard";
 
 describe("Dashboard section", () => {
 	test("show all widgets", async () => {
 		render(<Dashboard></Dashboard>);
 
-		const firstWidgetTitle = `Widget heading 1`;
+		const firstWidgetTitle = jikanApiResponses.data[0].title;
 		const firstWidgetHeader = await screen.findByRole("heading", {
 			name: new RegExp(firstWidgetTitle, "i"),
 		});
