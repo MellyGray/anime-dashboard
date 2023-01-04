d41eec60904e7d7357817dd5f75b2828c003dc09
MellyGray <melinaabril@gmail.com>
2023-01-04 19:14:59 +0100

    Add AnimeWidget component

---
+++ src/sections/dashboard/AnimeWidget.tsx
@@ -0,0 +1,26 @@
+import { Anime } from "../../domain/Anime";
+import styles from "./AnimeWidget.module.scss";
+
+export function AnimeWidget({ anime }: { anime: Anime }) {
+	return (
+		<article className={styles.container} key={anime.id}>
+			<header className={styles.container__header}>
+				<a
+					className={styles.container__title}
+					href={anime.url}
+					target="_blank"
+					title={anime.title}
+					rel="noreferrer"
+				>
+					<h2 className={styles.container__title}>{anime.title}</h2>
+				</a>
+			</header>
+			<div className={styles.container__body}>
+				<img src={anime.imageUrl} alt={anime.title} className={styles.container__image}></img>
+			</div>
+			<footer className={styles.container__footer}>
+				<span className={styles.container__text}>Score {anime.score}</span>
+			</footer>
+		</article>
+	);
+}
--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { AnimeRepository } from "../../domain/AnimeRepository";
+import { AnimeWidget } from "./AnimeWidget";
 import { Banner } from "./Banner";
 import styles from "./Dashboard.module.scss";
 import { useAnimes } from "./useAnimes";
@@ -16,30 +17,8 @@ export function Dashboard({ animeRepository }: { animeRepository: AnimeRepositor
 					</div>
 				) : (
 					<div className={styles.widgets_container}>
-						{animes.map((widget) => (
-							<article className={styles.widget} key={widget.id}>
-								<header className={styles.widget__header}>
-									<a
-										className={styles.widget__title}
-										href={widget.url}
-										target="_blank"
-										title={widget.title}
-										rel="noreferrer"
-									>
-										<h2 className={styles.widget__title}>{widget.title}</h2>
-									</a>
-								</header>
-								<div className={styles.widget__body}>
-									<img
-										src={widget.imageUrl}
-										alt={widget.title}
-										className={styles.widget__image}
-									></img>
-								</div>
-								<footer className={styles.widget__footer}>
-									<span className={styles.widget__text}>Score {widget.score}</span>
-								</footer>
-							</article>
+						{animes.map((anime) => (
+							<AnimeWidget anime={anime} />
 						))}
 					</div>
 				)}
