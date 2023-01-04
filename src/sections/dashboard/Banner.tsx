7128671fafc80971d7d224f7f5277a77587a2e69
MellyGray <melinaabril@gmail.com>
2023-01-04 18:58:32 +0100

    Create Sidebar component

--- src/sections/dashboard/Banner.tsx
+++ src/sections/dashboard/Banner.tsx
@@ -1,21 +1,12 @@
 import { ReactComponent as AnimeBanner } from "./anime-banner.svg";
 import styles from "./Banner.module.scss";
+import { Sidebar } from "./Sidebar";
 
 export function Banner() {
 	return (
 		<div className={styles.container}>
 			<AnimeBanner className={styles.image} />
-			<div className={styles.sidebar}>
-				<header className={styles.sidebar__header}>
-					<h2 className={styles.sidebar__title}>Header</h2>
-				</header>
-				<div className={styles.sidebar__body}>
-					<span className={styles.sidebar__text}>Image</span>
-				</div>
-				<footer className={styles.sidebar__footer}>
-					<span className={styles.sidebar__text}>Footer</span>
-				</footer>
-			</div>
+			<Sidebar />
 		</div>
 	);
 }
